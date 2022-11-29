91629a9908c6f357b86b33262d0e204bb0703ef4
Andrew Cotter <cotter.andrew.p@gmail.com>
2022-11-28 21:12:39 -0500

    class example

--- week11/p5BLE/ble-arduino/ble-arduino.ino
+++ week11/p5BLE/ble-arduino/ble-arduino.ino
@@ -1,19 +1,21 @@
 #include <ArduinoBLE.h>
 
 const int buttonPin = 4; // set buttonPin to digital pin 4
+const int potPin = A0;
 
-BLEService nanoService("add your service uuid here"); // create service
+BLEService nanoService("5db0217f-6715-4e63-b3d8-656d050233d6"); // create service
 
 // create button characteristic and allow remote device to get notifications
-BLEIntCharacteristic buttonCharacteristic("add your characteristic uuid here", BLERead | BLENotify);
+BLEIntCharacteristic buttonCharacteristic("71d1847d-412d-4554-aef0-7cf5210d9504", BLERead | BLENotify);
+BLEIntCharacteristic potCharacteristic("1e1011ce-a6a9-4bde-b4dc-ed561745c55a", BLERead | BLENotify);
 
 int sensorValue = 255;
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
+//  while (!Serial);
 
-  pinMode(ledPin, OUTPUT); // use the LED as an output
+//  pinMode(ledPin, OUTPUT); // use the LED as an output
   pinMode(buttonPin, INPUT); // use button pin as an input
   pinMode(potPin, INPUT);
 
@@ -31,11 +33,13 @@ void setup() {
 
   // add the characteristics to the service
   nanoService.addCharacteristic(buttonCharacteristic);
+  nanoService.addCharacteristic(potCharacteristic);
 
   // add the service
   BLE.addService(nanoService);
 
   buttonCharacteristic.writeValue(0);
+  potCharacteristic.writeValue(0);
 
   // start advertising
   BLE.advertise();
@@ -51,6 +55,10 @@ void loop() {
     sensorValue = digitalRead(buttonPin);
     Serial.println(sensorValue);
     buttonCharacteristic.writeValue(sensorValue);
+
+    int potValue = analogRead(potPin);
+    Serial.println(potValue);
+    potCharacteristic.writeValue(potValue);
 
     delay(1);
   }
--- week11/p5BLE/script.js
+++ week11/p5BLE/script.js
@@ -1,12 +1,15 @@
 
 // The serviceUuid must match the serviceUuid of the device you would like to connect
 // https://www.uuidgenerator.net/
-const serviceUuid = "add your service uuid here, make sure it matches your aduino's service uuid";
+const serviceUuid = "5db0217f-6715-4e63-b3d8-656d050233d6";
 let myBLE;
 let isConnected = false;
 
 let buttonCharacteristic;
 let buttonValue;
+
+let potCharacteristic;
+let potValue;
 
 function setup() {
   // Create a p5ble class
@@ -54,22 +57,37 @@ function gotCharacteristics(error, characteristics) {
   myBLE.onDisconnected(onDisconnected)
 
   // assign characteristic and value read callback
-  buttonCharacteristic = characteristics[0];
+  // buttonCharacteristic = characteristics[0];
+  for (let i = 0; i < characteristics.length; i++) {
+    if(characteristics[i].uuid === "71d1847d-412d-4554-aef0-7cf5210d9504") {
+      buttonCharacteristic = characteristics[i]
+    } else if (characteristics[i].uuid === "1e1011ce-a6a9-4bde-b4dc-ed561745c55a") {
+      potCharacteristic = characteristics[i]
+    }
+  }
 
   // Read the value of the first characteristic
   myBLE.read(buttonCharacteristic, gotButtonValue);
+  myBLE.read(potCharacteristic, gotPotValue);
 }
 
 // A function that will be called once got values
 function gotButtonValue(error, value) {
   if (error) console.log('error: ', error);
-  console.log('value: ', value);
+  console.log('button value: ', value);
   buttonValue = value;
   // After getting a value, call p5ble.read() again to get the value again
   myBLE.read(buttonCharacteristic, gotButtonValue);
   // You can also pass in the dataType
   // Options: 'unit8', 'uint16', 'uint32', 'int8', 'int16', 'int32', 'float32', 'float64', 'string'
   // myBLE.read(myCharacteristic, 'string', gotValue);
+}
+
+function gotPotValue(error, value) {
+  if (error) console.log('error: ', error)
+  console.log('pot value: ', value)
+  potValue = value
+  myBLE.read(potCharacteristic, gotPotValue)
 }
 
 function draw() {
@@ -82,7 +100,19 @@ function draw() {
   }
 
   if(buttonValue || buttonValue === 0){
+    
+
+
+    if (buttonValue == 1) {
+      ellipse(100, 100, potValue, potValue)
+    } else {
+      rectMode(CENTER)
+      rect(100, 100, potValue, potValue)
+    }
+
     // Write value on the canvas
     text(buttonValue, 100, 100);
   }
+
+
 }
